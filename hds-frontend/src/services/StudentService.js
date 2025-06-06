65267accf6ff33f76beb61058e864875665c5716
Karinho30000 <93045001+Karinho30000@users.noreply.github.com>
2025-06-07 00:37:23 +0200

    Update StudentService.js

--- hds-frontend/src/services/StudentService.js
+++ hds-frontend/src/services/StudentService.js
@@ -5,11 +5,11 @@ const REST_API_BASE_URL = `${API_BASE_URL}/api/students`;
 const REST_API_GROUPS_URL = `${API_BASE_URL}/api/groups`;
 const REST_API_LOCATIONS_URL = `${API_BASE_URL}/api/locations`;
 
-export const listStudents = () => axios.get(REST_API_BASE_URL);
-export const createStudent = (student) => axios.post(REST_API_BASE_URL, student);
-export const getStudent = (studentId) => axios.get(`${REST_API_BASE_URL}/${studentId}`);
-export const updateStudent = (studentId, student) => axios.put(`${REST_API_BASE_URL}/${studentId}`, student);
-export const deleteStudent = (studentId) => axios.delete(`${REST_API_BASE_URL}/${studentId}`);
+export const listStudents = () => axios.get(REST_API_BASE_URL, { withCredentials: true });
+export const createStudent = (student) => axios.post(REST_API_BASE_URL, student, { withCredentials: true });
+export const getStudent = (studentId) => axios.get(`${REST_API_BASE_URL}/${studentId}`, { withCredentials: true });
+export const updateStudent = (studentId, student) => axios.put(`${REST_API_BASE_URL}/${studentId}`, student, { withCredentials: true });
+export const deleteStudent = (studentId) => axios.delete(`${REST_API_BASE_URL}/${studentId}`, { withCredentials: true });
 
-export const listGroups = () => axios.get(REST_API_GROUPS_URL);
-export const listLocations = () => axios.get(REST_API_LOCATIONS_URL);
+export const listGroups = () => axios.get(REST_API_GROUPS_URL, { withCredentials: true });
+export const listLocations = () => axios.get(REST_API_LOCATIONS_URL, { withCredentials: true });
