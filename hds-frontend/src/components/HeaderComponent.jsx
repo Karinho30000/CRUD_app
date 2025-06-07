dd35174f46c02487788e5a061d44e76d4261cf70
Karinho30000 <93045001+Karinho30000@users.noreply.github.com>
2025-06-07 15:03:28 +0200

    Update HeaderComponent.jsx

--- hds-frontend/src/components/HeaderComponent.jsx
+++ hds-frontend/src/components/HeaderComponent.jsx
@@ -1,34 +1,34 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import LogoutButton from './LogoutButton';
 
 const HeaderComponent = () => {
-
   const navigator = useNavigate();
 
   return (
-    <div>
-        
-    	<header>
-            <nav className='navbar navbar-dark px-3' 
-                 style={{backgroundColor: 'deepskyblue'}}>
-              <button
-                className='btn btn-outline-light'
-                onClick={() => navigator('/teachers')}
-              >
-                All Teachers
-              </button>
-              <img
-                src="/hd_logo.png"
-                alt="Logo"
-                className="border border-dark-subtle"
-                style={{ height: '40px', }}
-              />
-            </nav>
+    <header>
+      <nav className="navbar navbar-dark px-3" style={{ backgroundColor: 'deepskyblue' }}>
+        <div className="container-fluid d-flex justify-content-between align-items-center w-100">
+          <button
+            className="btn btn-outline-light"
+            onClick={() => navigator('/teachers')}
+          >
+            All Teachers
+          </button>
 
-        </header>
+          <div className="d-flex align-items-center">
+            <LogoutButton />
+            <img
+              src="/hd_logo.png"
+              alt="Logo"
+              className="border border-dark-subtle ms-3" 
+              style={{ height: '40px' }}
+            />
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-    </div>
-  )
-}
-
-export default HeaderComponent
+export default HeaderComponent;
