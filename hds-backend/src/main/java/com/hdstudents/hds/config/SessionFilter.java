42a66d6f75912ca821946e25ca0bb5f60b662b36
Karinho30000 <93045001+Karinho30000@users.noreply.github.com>
2025-06-07 02:20:31 +0200

    Update SessionFilter.java

--- hds-backend/src/main/java/com/hdstudents/hds/config/SessionFilter.java
+++ hds-backend/src/main/java/com/hdstudents/hds/config/SessionFilter.java
@@ -16,15 +16,6 @@ public class SessionFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException{
-        response.setHeader("Access-Control-Allow-Origin", "https://helendoron-students.netlify.app");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization");
-
-        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-            response.setStatus(HttpServletResponse.SC_OK);
-            return;
-        }
 
         String path = request.getRequestURI();
         logger.info(path);
