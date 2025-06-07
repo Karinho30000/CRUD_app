2f8db17c8211bea279d6f1f0e45d60193b049a23
Karinho30000 <93045001+Karinho30000@users.noreply.github.com>
2025-06-07 12:02:16 +0200

    Update WebConfig.java

--- hds-backend/src/main/java/com/hdstudents/hds/config/WebConfig.java
+++ hds-backend/src/main/java/com/hdstudents/hds/config/WebConfig.java
@@ -13,9 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                                "https://helendoron-students.netlify.app"
-                        )
+                        .allowedOrigins("https://helendoron-students.netlify.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
