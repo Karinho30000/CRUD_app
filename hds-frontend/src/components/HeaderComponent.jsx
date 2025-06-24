5972d416e318d14986601e37f24d1cb612303c14
Karinho30000 <93045001+Karinho30000@users.noreply.github.com>
2025-06-24 13:44:12 +0200

    Update HeaderComponent.jsx

--- hds-frontend/src/components/HeaderComponent.jsx
+++ hds-frontend/src/components/HeaderComponent.jsx
@@ -21,7 +21,7 @@ const HeaderComponent = () => {
             <img
               src="/hd_logo_2.png"
               alt="Logo"
-              style={{ height: '50px', marginLeft: '20px' }}
+              style={{ height: '60px', marginLeft: '30px' }}
             />
           </div>
         </div>
